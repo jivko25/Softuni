22fc82c66d65deb337ff3c3d63558d7514dcdfe8
Jivko Uzunov <59103450+jivko25@users.noreply.github.com>
2020-10-01 14:48:06 +0300

    new

--- src/Softuni/Fundamentals/Arrays/ZigZagArrays.java
+++ src/Softuni/Fundamentals/Arrays/ZigZagArrays.java
@@ -1,24 +1,32 @@
 package Softuni.Fundamentals.Arrays;
-
 import java.util.Scanner;
 
 public class ZigZagArrays {
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
-		int[] arr = new int[n * 2];
-		int[] narr = new int[n * 2];
-		int[] karr = new int[n * 2];
+		String [] arr1 = new String [n];
+		String [] arr2 = new String [n];
 		for(int i = 0;i<n;i++) {
-			arr[i] = Integer.parseInt(sc.nextLine());
-			arr[n+i] = Integer.parseInt(sc.nextLine());
+			String content = sc.nextLine();
+			int space = content.indexOf(" ");
+			String firstPart = content.substring(0, space);
+			String secondPart = content.substring(space+1, content.length());
+			if(i%2 == 0) {
+			arr1[i] = firstPart;
+			arr2[i] = secondPart;
+			}
+			else {
+			arr2[i] = firstPart;
+			arr1[i] = secondPart;
+			}
 		}
 		for(int i = 0;i<n;i++) {
-			narr[i] = arr[i];
-			karr[i+1] = arr[i+1];
-			karr[i+2] = arr[i+2];
-			narr[i+3] = arr[i+3];
+			System.out.print(arr1[i] + " ");
 		}
-		System.out.println(narr + " " + karr);
+		System.out.println();
+		for(int i = 0;i<n;i++) {
+			System.out.print(arr2[i] + " ");
+		}
 	}
 }
