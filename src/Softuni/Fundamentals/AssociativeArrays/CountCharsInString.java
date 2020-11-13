88f48dc58350297487b61252c5940e488eb2deb8
jivko25 <jivkou@yahoo.com>
2020-11-13 16:17:04 +0200

    Update CountCharsInString.java

--- src/Softuni/Fundamentals/AssociativeArrays/CountCharsInString.java
+++ src/Softuni/Fundamentals/AssociativeArrays/CountCharsInString.java
@@ -1,24 +1,27 @@
 package Softuni.Fundamentals.AssociativeArrays;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CountCharsInString {
 	public static void main(String [] args) {
 		Scanner sc = new Scanner(System.in);
 		String word = sc.nextLine();
-		HashMap<String, Integer> chars = new HashMap<String, Integer>();
+		Map<String, Integer> chars = new HashMap<>();
 		String [] arr = word.split("");
 		for(int i = 0;i<arr.length;i++) {
-//		if(chars.get(arr[i]) == null) {
-//			chars.put(arr[i], 1);
-//		}
-//		else {
-			chars.put(arr[i], chars.get(arr[i])+ 1);
-//		}
-		for (String n : chars.keySet()) {
-			  System.out.println(n);
-			}
+		if(chars.get(arr[i]) == null) {
+			chars.put(arr[i], 1);
 		}
+		else {
+			chars.put(arr[i], chars.get(arr[i])+ 1);
+		}
+		}
+		for (String n : chars.keySet()) {
+			String key = n.toString();
+            String value = chars.get(n).toString();  
+            System.out.println(key + " -> " + value); 
+			}
 	}
 }
