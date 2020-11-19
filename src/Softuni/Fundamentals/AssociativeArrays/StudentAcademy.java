a1c33fb2e281999cf2a6f325c8052d48ca376115
jivko25 <jivkou@yahoo.com>
2020-11-20 01:16:09 +0200

    Update StudentAcademy.java

--- src/Softuni/Fundamentals/AssociativeArrays/StudentAcademy.java
+++ src/Softuni/Fundamentals/AssociativeArrays/StudentAcademy.java
@@ -6,32 +6,26 @@ public class StudentAcademy {
 	public static void main(String [] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
-		List<String> names = new ArrayList<>();
-		List<Double> grades = new ArrayList<>();
-		Map<String, Double> avr = new HashMap<>();
-		Map<String, Integer> count = new HashMap<>();
-		Map<String, Double> sum = new HashMap<>();
-		for(int i = 0; i < n*2; i++) 
-		{
-			if(i%2 ==0) 
-			{
-				String name = sc.nextLine();
-				names.add(name);
-			}
-			else 
-			{
-				Double grade = Double.parseDouble(sc.nextLine());
-				grades.add(grade);
-			}
+		List<String> input = new ArrayList<>();
+		Map<String, Double> grades = new HashMap<>();
+		Map<String, Integer> counter = new HashMap<>();
+		for(int i = 0;i<n*2;i++) {
+			String in = sc.nextLine();
+			input.add(in);
 		}
-		for(int i = 0;i<n;i++) 
-		{
-			if(avr.get(names.get(i)) == null) {
-				count.put(names.get(i), 1);
+		for(int i = 0;i<n*2;i+=2) {
+			if(grades.get(input.get(i)) == null) {
+				grades.put(input.get(i), Double.parseDouble(input.get(i+1)));
+				counter.put(input.get(i), 1);
 			}
 			else {
-				count.put(names.get(i), count.get(names.get(i)+1));
+				grades.put(input.get(i), grades.get(input.get(i)) + Double.parseDouble(input.get(i+1)));
+				counter.put(input.get(i), counter.get(input.get(i)) + 1);
 			}
 		}
+		grades.entrySet().stream()
+		.filter(e -> e.getValue()/counter.get(e.getKey()) >= 4.5 )
+		.sorted((a,b) -> Double.compare(a.getValue()/counter.get(a.getKey()),b.getValue()/counter.get(b.getKey())))
+		.forEach(d -> System.out.printf("%s -> %.2f",d.getKey(),d.getValue()/counter.get(d.getKey())));;
 	}
 }
