6fea168029b91ee9680ae5b85ae16fbb43919761
jivko25 <jivkou@yahoo.com>
2020-11-25 00:04:20 +0200

    Update ValidUsernames.java

--- src/Softuni/Fundamentals/TextProcessing/ValidUsernames.java
+++ src/Softuni/Fundamentals/TextProcessing/ValidUsernames.java
@@ -5,32 +5,24 @@ import java.util.Scanner;
 public class ValidUsernames {
 	public static void main(String [] args) {
 		Scanner sc = new Scanner(System.in);
-		boolean isEnd = false;
-		boolean isFail = false;
-		String input = sc.nextLine();
-		String [] bigarr = input.split(", ");
-		while(isEnd == false) {
-			isFail = false;
-			for(int k = 0;k<bigarr.length;k++) {
-			char [] arr = bigarr[k].toCharArray();
-			for(int i = 0;i<arr.length;i++) {
-				int val = arr[i];
-				if(((val>=48 && val<=57) || (val>=65 && val<=90) || (val>=97 && val<=122) || '_' == arr[i] ) && (arr.length>=3 && arr.length<=16)) {
-				}
-				else {
-					isFail = true;
-				}
-				if(isFail == true) {
-					break;
-				}
-			}
-			if(isFail == true) {
-				continue;
-			}
-			else if(isFail == false){
-			System.out.println(input);
-			}
+		String [] arr = sc.nextLine().split(", ");
+		for(int i = 0; i<arr.length;i++) {
+			if(isUsernameValid(arr[i])) {
+				System.out.println(arr[i]);
 			}
 		}
+	}
+	
+	static boolean isUsernameValid(String input) {
+		if(input.length()<3 || input.length()>16) {
+			return false;
+		}
+		for(int i = 0;i<input.length();i++) {
+			char part = input.charAt(i);
+			if(!Character.isLetterOrDigit(part) && part != '_' && part != '-') {
+				return false;
+			}
+		}
+		return true;
 	}
 }
