85232499a9c1de2be180376fe38f51c8b0e665a2
jivko25 <jivkou@yahoo.com>
2020-11-26 21:55:58 +0200

    Create MultiplyBigNumber.java

---
+++ src/Softuni/Fundamentals/TextProcessing/MultiplyBigNumber.java
@@ -0,0 +1,15 @@
+package Softuni.Fundamentals.TextProcessing;
+
+import java.util.Scanner;
+
+public class MultiplyBigNumber {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		int b = Integer.parseInt(sc.nextLine());
+		String res = "";
+		for(int i = 0;i<a.length();i++) {
+			res 
+		}
+	}
+}
