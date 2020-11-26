fef404ba83dec5b1d8a88d1eceb12acb00681cae
jivko25 <jivkou@yahoo.com>
2020-11-27 01:27:45 +0200

    Update MultiplyBigNumber.java

--- src/Softuni/Fundamentals/TextProcessing/MultiplyBigNumber.java
+++ src/Softuni/Fundamentals/TextProcessing/MultiplyBigNumber.java
@@ -7,9 +7,26 @@ public class MultiplyBigNumber {
 		Scanner sc = new Scanner(System.in);
 		String a = sc.nextLine();
 		int b = Integer.parseInt(sc.nextLine());
-		String res = "";
-		for(int i = 0;i<a.length();i++) {
-			res 
+		StringBuilder sb = new StringBuilder();
+		int carry = 0;
+		int res = 0;
+		if(b == 0) {
+			System.out.println(0);
+			return;
 		}
+		for(int i = a.length() - 1;i>=0;i--) {
+			int digit = Integer.parseInt(String.valueOf(a.charAt(i)));
+			int result = digit * b + carry;
+			
+			if(i==0) {
+				sb.insert(0,  result);
+			}
+			else {
+				int resDigit = result%10;
+				carry = result/10;
+				sb.insert(0,resDigit);
+			}
+		}
+		System.out.println(sb);
 	}
 }
