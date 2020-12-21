f09a8b02f782ddf8f38d229cecf6a7221ac5b3a1
jivko25 <jivkou@yahoo.com>
2020-12-21 18:41:21 +0200

    Create BasicStackOperations.java

---
+++ src/Softuni/JavaAdvanced/StacksAndQueues/BasicStackOperations.java
@@ -0,0 +1,34 @@
+package Softuni.JavaAdvanced.StacksAndQueues;
+
+import java.util.*;
+
+public class BasicStackOperations {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<Integer> stack = new Stack<Integer>();
+		String [] NSX = sc.nextLine().split(" ");
+		String [] arr = sc.nextLine().split(" ");
+		int min = 5000000;
+		boolean isStackContainElement = false;
+		for (int i = 0; i < Integer.parseInt(NSX[0]); i++) {
+			stack.add(Integer.parseInt(arr[i]));		
+		}
+		for (int i = 0; i < Integer.parseInt(NSX[1]); i++) {
+			stack.pop();		
+		}
+		for (int i = 0; i < stack.size(); i++) {
+			if(stack.peek()<min) {
+				min = stack.peek();
+			}
+			if(stack.contains(Integer.parseInt(NSX[2]))) {
+				System.out.print(stack.contains(Integer.parseInt(NSX[2])));
+				isStackContainElement = true;
+				break;
+			}
+			stack.pop();
+		}
+		if(!isStackContainElement) {
+			System.out.println(min);
+		}
+	}
+}
