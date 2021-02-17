8ac0002827a6ea4a90455866e995643cdbe70c14
jivko25 <jivkou@yahoo.com>
2021-02-17 14:20:49 +0200

    update

---
+++ src/Sofuni/JavaAdvanced/MultidimensionalArrays/MatrixOfPalindromes.java
@@ -0,0 +1,34 @@
+package Sofuni.JavaAdvanced.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class MatrixOfPalindromes {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		String [] input = sc.nextLine().split("\\s+");
+		StringBuilder [][] arr = new StringBuilder[Integer.parseInt(input[0])][Integer.parseInt(input[1])];
+		int pattern = 97;
+		for(int x = 0;x<Integer.parseInt(input[0]); x++) {
+			int temp = pattern + x;
+			for(int y = 0;y<Integer.parseInt(input[1]); y++) {
+				StringBuilder sb = new StringBuilder();
+				char tempChar = (char) (pattern+y);
+				sb.append(tempChar);
+				char newChar = (char) (temp+y);
+				sb.append(newChar);
+				sb.append(tempChar);
+				arr[x][y] = sb;
+			}
+		}
+		printMatrix(arr, Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+	}
+	
+	public static void printMatrix(StringBuilder [][] arr, int sizeX, int sizeY) {
+		for(int y = 0;y<sizeY;y++) {
+			for(int x = 0;x<sizeX;x++) {
+					System.out.print(arr[x][y] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
